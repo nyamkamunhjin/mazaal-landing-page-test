c8981532e2753ede292728d92bf871e82e8088d7
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2024-06-18 14:18:07 +0800

    add webpack bundle analyzer

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: ["geist"],
-  siteUrl: `https://${process.env.URL}`,
-  generateRobotsTxt: true,
-};
-
-export default nextConfig;
