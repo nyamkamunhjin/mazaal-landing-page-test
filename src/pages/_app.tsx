bae66de4849a9cb1757dc1151646b1b139433c74
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2024-06-18 13:41:25 +0800

    add meta to head element

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,13 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { GeistSans } from 'geist/font/sans';
 import { myFont } from '../styles/fonts';
-import { useReportWebVitals } from 'next/web-vitals';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App({ Component, pageProps }: AppProps) {
-    useReportWebVitals((metric) => {
-        console.log(metric);
-    });
-
     return (
         <main className={`${GeistSans.className} ${myFont.variable}`}>
             <Component {...pageProps} />
+            <SpeedInsights />
         </main>
     );
 }
